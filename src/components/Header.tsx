5dc57facaf0bbe5e08648e32df7c686bd82cbacc
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2026-05-23 23:03:32 +0530

    Design the header reusable component & use it

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+interface HeaderProps {
+  title?: string;
+  headerStyle?: any;
+  containerStyle?: any;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  title,
+  headerStyle,
+  containerStyle,
+}) => {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Text style={[styles.headerText, headerStyle]}>{title}</Text>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: '#f8f8f8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
