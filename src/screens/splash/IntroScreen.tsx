0e7d7364b5b8f68d2f8c89ba45efdb0b32535e05
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2026-06-05 22:30:30 +0530

    created the start screen for the app & add animated smooth logo

--- src/screens/splash/IntroScreen.tsx
+++ src/screens/splash/IntroScreen.tsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Animated } from 'react-native';
+import React, { FC, useRef, useEffect } from 'react';
+import { navigate } from '../../utils/NavigationUtil';
 
-const IntroScreen = () => {
+const IntroScreen: FC = () => {
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
+
+  useEffect(() => {
+    Animated.timing(scaleAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    setTimeout(() => {
+      // Navigate to onboard screen after animation
+      navigate('OnboardScreen');
+    }, 1500);
+  }, [scaleAnim]);
+
   return (
-    <View>
-      <Text>IntroScreen</Text>
+    <View style={styles.container}>
+      <Animated.Image
+        source={require('../../assets/images/logo1.png')}
+        style={[
+          styles.logo,
+          {
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default IntroScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 420,
+    height: 320,
+    resizeMode: 'contain',
+  },
+});
