408645a24b4235e7978ea2c9e56aa6a208344240
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2026-06-06 15:06:00 +0530

    create dummy onboard screen

--- src/screens/splash/OnboardingScreen.tsx
+++ src/screens/splash/OnboardingScreen.tsx
@@ -1,12 +1,60 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import React, { FC } from 'react';
 
-const OnboardScreen = () => {
+const OnboardingScreen: FC = () => {
   return (
-    <View>
-      <Text>OnboardScreen</Text>
+    <View style={styles.container}>
+      {/* Heading area */}
+      <View style={styles.heading}>
+        <TouchableOpacity>
+          <Text style={styles.headingContent}>SKIP</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Main content area */}
+      <View style={styles.mainContent}>
+        <Text>Main Content</Text>
+      </View>
+
+      {/* Footer area content will go here */}
+      <View style={styles.footer}>
+        <Button title=" BACK" onPress={() => console.log('Pressed')} />
+        <Button title="NEXT" onPress={() => console.log('Pressed')} />
+      </View>
     </View>
   );
 };
 
-export default OnboardScreen;
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 5,
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  heading: {
+    flex: 0.05,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'blue',
+  },
+  headingContent: {
+    fontSize: 18,
+  },
+  mainContent: {
+    flex: 0.8,
+    borderWidth: 1,
+    borderColor: 'green',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 0.1,
+    borderWidth: 1,
+    borderColor: 'green',
+  },
+});
